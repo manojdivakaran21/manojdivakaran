f0e862cf149bf950165d0308fc9eff3806835fd7
manojdivakaran21 <manojdivakaran138@gmail.com>
2016-10-09 15:42:01 +0530

    First Full code commit

---
+++ app/src/main/java/com/example/shivaprakashys/heavenly_kerala/fourthFragment.java
@@ -0,0 +1,21 @@
+package com.example.shivaprakashys.heavenly_kerala;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ShivaprakashY.S on 08-10-2016.
+ */
+public class fourthFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView= inflater.inflate(R.layout.beaches,container,false);
+        return myView;
+    }
+}
